16340c64f746989cc6f3798b6a746c774a33fe04
c0a25054f8 <c0a25054f8@edu.teu.ac.jp>
2026-04-14 17:02:51 +0900

    演習1:こうかとんを右矢印キーで動かす

--- pygame_image.py
+++ pygame_image.py
@@ -16,7 +16,7 @@ def main():
     kk_img = pg.transform.flip(kk_img, True, False)
 
     kk_rct = kk_img.get_rect() #こうかとんRectの取得
-    kk_rct.center = 300,200
+    
 
     tmr = 0
     while True:
@@ -24,21 +24,19 @@ def main():
             if event.type == pg.QUIT: return
         
         key_lst = pg.key.get_pressed()
-        if key_lst[pg.K_UP]:
-            kk_rct.move_ip((0,-1))
-        if key_lst[pg.K_DOWN]:
-            kk_rct.move_ip((0,1))
+        mx=-1
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip((1,0))
-        if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip((-1,0))
+            mx = 1
+        kk_rct.move_ip((mx, 0))
 
         x=tmr%3200 #練習5
         screen.blit(bg_img, [-x, 0]) #練習2
         screen.blit(bg_img2, [-x+1600, 0]) #練習7
         screen.blit(bg_img, [-x+3200, 0]) #練習9
 
-        screen.blit(kk_img, kk_rct) #練習4,10
+        # kk_rct.center = -x+300,200
+        screen.blit(kk_img,kk_rct) #練習4,10
+
         pg.display.update()
         tmr += 1        
         clock.tick(200) #練習6
